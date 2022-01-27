ffb3f0ba13208aa6c0278263c56f90aa659247ed
Javier Ramirez <apollo.infinity88@gmail.com>
2022-01-27 15:49:01 -0800

    Removed some code. Removed routes, express instance, and some middleware and moved them to app.ts, so that it'll be easier to test

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import todoRouter from './todo/router';
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/v1', todoRouter);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-import express from 'express';
+import app from './app';
 
 import db from './config/database.config';
-
-import todoRouter from './todo/router';
 
 db.sync().then(() => {
 	console.log('connect to DB');
 });
 
-const app = express();
 const port = 9000;
-
-app.use(express.json());
-app.use('/', todoRouter);
 
 app.listen(port, () => {
 	console.log(`Server working port: ${port}`);
 });
+
+export default app;
